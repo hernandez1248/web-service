e18e28c95c07a36f9e32b49fc790ecdd2b9b66c4
relsonGR <riverarelson99@gmail.com>
2023-06-24 13:29:03 -0600

    Se suben avances de la BD de orderDetails

---
+++ database/migrations/20230624192131-create-order-details.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('OrderDetails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      amountTotal: {
+        type: Sequelize.STRING
+      },
+      quantityComponent: {
+        type: Sequelize.STRING
+      },
+      unitPrice: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('OrderDetails');
+  }
+};
