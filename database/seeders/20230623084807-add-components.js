9ad0ac4183c3a2fb1185657c22a8daab4ceb2ef7
Relson Antonio <94725840+relsonGR@users.noreply.github.com>
2023-07-01 18:11:09 -0600

    correccion

--- database/seeders/20230623084807-add-components.js
+++ database/seeders/20230623084807-add-components.js
@@ -5,9 +5,9 @@ module.exports = {
   async up (queryInterface, Sequelize) {
 
     return queryInterface.bulkInsert('Components', [
-      {name: 'Batería 4.000 mAh', price: 189, stock: 24, deviceId: 1, createdAt: new Date(), updatedAt: new Date()},
-      {name: 'Pantalla 6.2 HD+', price: 399, stock: 20, deviceId: 2, createdAt: new Date(), updatedAt: new Date()},
-      {name: 'Cámara trasera 13MP f/1.9', price: 88, stock: 54, deviceId: 3, createdAt: new Date(), updatedAt: new Date()},
+      {name: 'Batería 4.000 mAh', price: 189, stock: 24, createdAt: new Date(), updatedAt: new Date()},
+      {name: 'Pantalla 6.2 HD+', price: 399, stock: 20, createdAt: new Date(), updatedAt: new Date()},
+      {name: 'Cámara trasera 13MP f/1.9', price: 88, stock: 54, createdAt: new Date(), updatedAt: new Date()},
     ]);
   },
 
